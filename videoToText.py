678e541728a3310cc968fbeaf168e3ab7f8b4191
sophia walton <srw9rx@virginia.edu>
2025-04-29 20:52:15 -0400

    make tsv with all videos. for some reason it only gets 5

--- videoToText.py
+++ videoToText.py
@@ -4,9 +4,11 @@
 # youtube link: https://www.youtube.com/c/CityofMuskegonMeetings
 # https://naveen-malla.medium.com/transcribing-any-youtube-video-with-python-a-step-by-step-guide-cea7dd4e32f5
 
-from youtube-transcript-api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi
 import re
 import scrapetube
+import datetime
+import pandas as pd
 
 YOUTUBE_RE = r'youtube.com/watch\?v=([\w-]*)' # re to parse out the video id given the youtube link
 
@@ -18,18 +20,45 @@ def downloadFromId(yt_api, video_link=None, video_id=None):
     rawSnippets = snippets.to_raw_data()
     return rawSnippets
 
-def getAllTranscriptsForChannel(channelId, ytApi)
-    videos = scrapetube.get_channel(channelId)
+def getAllTranscriptsForChannel(channel_id, ytApi, cutoff_date=datetime.date(year=2022, day=3, month=1)):
+    videos = scrapetube.get_channel(channel_id)
+    videosDict = []
+    i = 0
     for video in videos:
-        print(video)
-        downloadFromId(yt_api=ytApi, video_id=video['videoId'])
-        #TODO: export these to a csv
-
+        i+=1
+        title = video['title']['runs'][0]['text']
+        datestring = None
+        try:
+            datestring = title.split()[0]
+            if '/' in datestring:
+                video_date = datetime.datetime.strptime(title.split()[0], '%m/%d/%y').date()
+            elif '-' in datestring:
+                video_date = datetime.datetime.strptime(title.split()[0], '%m-%d-%y').date()
+        except Exception:
+            video_date = datetime.date.today()
+        id = video['videoId']
+        if video_date >= cutoff_date:
+            snippets = downloadFromId(yt_api=ytApi, video_id=id)
+            for snippet in snippets:
+                snippetdict = {
+                    'title': title,
+                    'date': video_date,
+                    'video_id': id,
+                    'text': snippet['text'],
+                    'start': snippet['start'],
+                    'duration': snippet['duration']
+                }
+                videosDict.append(snippetdict)
+    return videosDict
+        
 
 
 
 if __name__ == '__main__':
     ytt_api = YouTubeTranscriptApi()
-    channelId = 'UCI0JV91vepm1gAi6_O-5' # this can be obtained by view page source and searching for channelId
-    transcripts = getAllTransciptsForChannel(channelId, ytt_api)
+    #TODO: get channelID from selenium or bs4
+    channelId = 'UCI0JV91vepm1gAi6_O-5Nlg' # this can be obtained by view page source and searching for channelId
+    transcripts = getAllTranscriptsForChannel(channelId, ytt_api)
+    df = pd.DataFrame(transcripts)
+    df.to_csv('all_transcripts_by_snippet.tsv', sep='\t')
     
