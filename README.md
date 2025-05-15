b651df047087a3c9fdc0902eacfa877e8c33786b
sophia walton <srw9rx@virginia.edu>
2025-05-14 20:21:20 -0400

    add basic clustering and ignore api key

---
+++ README.md
@@ -0,0 +1,4 @@
+**useful links:**  
+https://medium.com/@apparaomulpuri/speaker-diarization-in-python-a-step-by-step-guide-351a094237f2  with sklearn 
+https://github.com/manojpamk/pytorch_xvectors?tab=readme-ov-file a torch library  
+https://github.com/wq2012/awesome-diarization?tab=readme-ov-file  a compilation of speech diarization
---
+++ videoToWav.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+import yt_dlp
+import ffmpeg
+import sys
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+#    'outtmpl': 'output.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+    }],
+}
+def download_from_url(url):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        stream = ffmpeg.input('output.m4a')
+        stream = ffmpeg.output(stream, 'data/output.wav')
+
+
+
+
+if __name__ == '__main__':
+    url = 'https://www.youtube.com/watch?v=OeFJ4qCVDgI&ab_channel=CityofMuskegonMeetings'
+    download_from_url(url)
